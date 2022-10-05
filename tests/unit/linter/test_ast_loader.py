5b3504fb8045fffd94a0a41004a87f1e5cdeec39
Sviat <wstswsb@gmail.com>
2022-10-05 14:55:25 +0300

    add AstLoader unit-test

---
+++ tests/unit/linter/test_ast_loader.py
@@ -0,0 +1,21 @@
+from mock import Mock, mock_open, patch
+from linter import AstLoader
+
+
+class TestAstLoader:
+    def setup(self):
+        self.ast_loader = AstLoader()
+
+    @patch('linter.ast_loader.ast')
+    @patch('builtins.open', new_callable=mock_open, read_data='print()')
+    def test_from_file(self, open_mock, ast_module_mock):
+        ast_mock = Mock()
+        ast_module_mock.parse.return_value = ast_mock
+        file_path = 'test-file-path'
+        
+        result = self.ast_loader.from_file(file_path)
+
+        assert result == ast_mock
+        ast_module_mock.parse.assert_called_once_with('print()')
+        open_mock.assert_called_once_with(file_path, 'r')
+        
